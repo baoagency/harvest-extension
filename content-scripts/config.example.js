7b792c41d49ac4763c0dce8e06fc3b915d24c6ee
Mike <mikemoore88@gmail.com>
2024-01-09 12:13:25 +0000

    Update example config

--- content-scripts/config.example.js
+++ content-scripts/config.example.js
@@ -2,18 +2,40 @@ window.BAO = window.BAO || {}
 
 window.BAO.HarvestExtension = {
   mode: 'production',
-  /* An array of Platforms requiring custom configuration */
   platforms: [
     {
-      /* The hostname of the platform eg basecamp.com, monday.com, asana.com */
-      hostname: '',
+      hostname: 'basecamp.com',
       selectors: {
-        /* An array of css selectors for elements which are able to contain the name of the task eg '.todo__title' */
-        /* The value of the <title> element is used by default */
-        task_title: [],
-        /* An array of css selectors for elements which are able to contain the name of the merchant eg 'meta[name="current-bucket-name"]' */
-        group_name: []
+        task_title: [
+          '.todo__title',
+          '.message__subject'
+        ],
+        group_name: [
+          'meta[name="current-bucket-name"]'
+        ]
       }
     },
+    {
+      hostname: 'monday.com',
+      selectors: {
+        task_title: [
+          '.heading-component',
+        ],
+        group_name: [
+          '.workspace-name-wrapper'
+        ]
+      }
+    },
+    {
+      hostname: 'asana.com',
+      selectors: {
+        task_title: [
+          '.TitleInput-objectName',
+        ],
+        group_name: [
+          'meta[name="shard_id"]'
+        ]
+      }
+    }
   ]
 }
